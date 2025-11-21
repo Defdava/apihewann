56688941cba95e3b4b9d30615355f656603400f0
Defdava <defdavaharyadi23@gmail.com>
2025-11-21 21:04:12 +0700

    first commit

--- api/index.js
+++ api/index.js
@@ -1,5 +1,10 @@
+// api/index.js
 import serverless from "serverless-http";
 import { app } from "../server.js";
 
-export const config = { runtime: "nodejs20" };
+// Hapus config atau gunakan runtime yang valid
+export const config = { 
+  runtime: "nodejs"  // Gunakan "nodejs" saja, bukan "nodejs20.x"
+};
+
 export default serverless(app);
